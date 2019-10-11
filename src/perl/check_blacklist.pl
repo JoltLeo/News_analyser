04ade3b79c3293d3a21da4db7f4dc2ea332f1ed4
gFrancoCamilo <gustavocamilo@poli.ufrj.br>
2019-10-11 20:11:36 -0300

    Updated check_blacklist.pl and check_artifacts.pl

--- src/perl/check_blacklist.pl
+++ src/perl/check_blacklist.pl
@@ -1,24 +1,23 @@
-#A funcao check_author abre o arquivo de noticias e o arquivo com os autores na blacklist e verifica se o autor da noticia esta na blacklist
+#Function that verifies if the news author/source is on the blacklist of authors/source. Returns a metric for future evaluation of seriousness'
+sub check_author_and_source{
 
-sub check_author{
-
-    #Recebe o nome do arquivo de noticia e o arquivo com a blacklist como argumento e abre os arquivos
+    #Receives news file and author blacklist file
     $news_file_name = $_[0];
     $blacklist_file_name = $_[1];
     $number_lines = $_[2];
-    open (my $news, "<", $news_file_name) or die "Nao foi posivel abrir o arquivo de noticias $news_file_name: $!\n";
-    open (my $blacklist, "<", $blacklist_file_name) or die "Nao foi possivel abrir o arquivo $blacklist_file_name: $!\n";
+    open (my $news, "<", $news_file_name) or die "Could not open news file $news_file_name: $!\n";
+    open (my $blacklist, "<", $blacklist_file_name) or die "Could not open blaklist file $blacklist_file_name: $!\n";
 
-    #Lê e descarta a primeira linha para pegar a segunda linha do texto, que contem o nome do autor
+    #Reads and discard the first and second line in order to get the author in the third line 
     my $line;
     my $index;
     for ($index = 0; $index < $number_lines; $index++){
         $line = <$news>;
     }
 
-    #Verifica como o arquivo informa o nome do autor, i.e, se na segunda linha está o nome do autor ou está 'Autor:' seguido do nome do autor.
+    #Verifies how the author is informed in the file
     if ($number_lines == 3){
-        if ($line =~ /[Aa]utor:/){
+        if ($line =~ /[Aa]utor:|[Aa]utor\s-/){
             @word = split /:\-/, $line;
             $line = @word[1];
         }
@@ -27,7 +26,7 @@ sub check_author{
     my $line2;
     my $counter = 0;
 
-    #Compara a as linhas da blacklist com a segunda linha para verificar se o autor esta na lista ou nao
+    #Compares each line from the blacklist with the authors line to verifie if author is marked or not
     while ($line2 = <$blacklist>) {
         if ($line =~ /$line2/ ){
             $counter = $counter + 1;
@@ -35,8 +34,8 @@ sub check_author{
     }
 
 
-    close $news or die "Nao foi possivel fechar o arquivo $news_file_name: $!\n";
-    close $blacklist or die "Nao foi possivel fechar o arquivo $blacklist_file_name: $!\n"
+    close $news or die "Could not close file $news_file_name: $!\n";
+    close $blacklist or die "Could not close file $blacklist_file_name: $!\n"
 
     return $counter;
 }
