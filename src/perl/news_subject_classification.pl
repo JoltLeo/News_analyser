c16add0cc02fe9441a3c83c67a87ee93322b4e83
JoltLeo <55643297+JoltLeo@users.noreply.github.com>
2019-10-14 14:31:10 -0300

    Create news_subject_classification.pl

---
+++ src/perl/news_subject_classification.pl
@@ -0,0 +1,34 @@
+pack
+use strict;
+use warnings;
+
+#Classification subjects:
+#(i) 'celebrity'
+#(ii) 'economy'
+#(iii) 'politics'
+#(iv) 'science'
+#(v) 'sports'
+#(vi) '\0' for undefined subject
+
+#Function that evaluate if it is a celebrity news. Returns a grade from 0 to 100 representing the chance of the news' 
+#subject be celebrity.
+sub check_celebrity_sunject{
+
+    #Receives news' file
+    $news_file_name = $_[0];
+    
+    #Checking if the format of the file is right
+    my $returnCheck = check_news_format($news_file_name);
+    if $returnCheck == -1{
+        return -1;
+    }
+    
+    #Trying to open news' file
+    open (my $news, "<", $news_file_name) or die "ERROR: Could not open file $news_file_name: $!\n";
+    
+    my $line;
+    my $index;
+    my $counter = 0;
+    
+
+    close $news or die "ERROR: Could not close file $news_file_name: $!\n";
