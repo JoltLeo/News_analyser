a22975a3bb38a3da20238854553e4c3e4410b8b1
root <root@ipanema.gta.ufrj.br>
2019-10-17 09:11:53 -0300

    Fixed errors in check_new_format.pm

--- src/perl/modules/check_news_format.pm
+++ src/perl/modules/check_news_format.pm
@@ -18,22 +18,40 @@ sub check_news_format{
     my $news_file_name = $_[0];
     open (my $news, "<", $news_file_name) or die "ERROR: Could not open file $news_file_name: $!\n";
 
+    #Verifies if there is at least one author in the news
     my $line;
     my $number_lines = 2;
     my $index;
     for ($index = 0; $index < $number_lines; $index++){
         $line = <$news>;
     }
-    if (not defined $line || $line eq ""){
+    chomp $line;
+    if ((not defined $line) || ($line eq "") || (length($line) == 0)){
         close $news or die "ERROR: Could not close file $news_file_name: $!\n";
         return -1;
     }
 
-    $number_lines = 2;
-    for ($index = 0; $index < $number_lines; $index++){
-        $line = <$news>;
+    #Matches date format in the third line of the news
+    my $counter = 0;
+    $line = <$news>;
+    if ($line =~ m/\d\d\/\d\d\/\d\d|\d\/\d\d\/\d\d|\d\/\d\/\d\d|\d\d\/\d\d\/\d\d\d\d|\d\/\d\d\/\d\d\d\d|\d\/\d\/\d\d\d\d/){
+        $counter ++;
     }
-    if (not defined $line || $line eq ""){
+    if ($line =~ m/\d\d\-\d\d\-\d\d|\d\-\d\d\-\d\d|\d\-\d\-\d\d|\d\d\-\d\d\-\d\d\d\d|\d\-\d\d\-\d\d\d\d|\d\-\d\-\d\d\d\d/){
+        $counter ++;
+    }
+    
+    if ($counter == 0){
+        close $news or die "ERROR: Could not close file $news_file_name: $!\n";
+        return -1;
+    }
+    
+    
+    #Verifies if there is a source in the news
+    $line = <$news>;
+    chomp $line;
+  
+    if ((not defined $line) || ($line eq "") || (length($line) == 0)){
         close $news or die "ERROR: Could not close file $news_file_name: $!\n";
         return -1;
     }
@@ -41,3 +59,8 @@ sub check_news_format{
     close $news or die "ERROR: Could not close file $news_file_name: $!\n";
     return 0;
 }
+
+#The commented part below was used for testing the code
+#my $news_file = "arquivo.txt";
+#my $return_value = check_news_format($news_file);
+#print $return_value,"\n";
