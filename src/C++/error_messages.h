3718402c0561562ecc107012dfb0a7abf0017743
JoltLeo <55643297+JoltLeo@users.noreply.github.com>
2019-11-22 14:24:20 -0300

    Fixed error_messages.h changing type string to const char* const

--- src/C++/error_messages.h
+++ src/C++/error_messages.h
@@ -1,10 +1,10 @@
 #ifndef ERROR_MESSAGES_H
 #define ERROR_MESSAGES_H
 
-#include <string>
+using namespace std;
 
 //All error messages from C++ and Perl
-string error_messages 
+const char * const error_messages [] = 
 {
     "The string is empty\n", // error empty string 
     "ERROR: Could not open news file on function show_news on show_news.cpp\n", // error -1
